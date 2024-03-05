4cb19f9742f6a55c37fc8e70aaa4b5c9dc340188
Leonid <kissmykrabeskill@gmail.com>
2024-03-05 19:47:22 +0500

    + task 3 refactor

--- des/home/Task3.py
+++ des/home/Task3.py
@@ -1,32 +1,23 @@
 class Solution(object):
-    def lengthOfLongestSubstring(self, s):
+    def lengthOfLongestSubstring(self, s: str) -> int:
         """
         :type s: str
         :rtype: int
         """
-        substring = ''
-        result_substring = ''
-        last_symbol = ''
-        if len(s) == 0:
-            return 0
-        for j in range(0, len(s)):
-            for i in s[j::]:
-                if substring == '':
-                    substring = i
-                elif i in substring:
-                    if len(substring) > len(result_substring):
-                        result_substring = substring
-                    substring = i
-                elif result_substring != '' and i == last_symbol:
-                    substring = i
-                elif i != substring[-1]:
-                    substring += i
-                last_symbol = i
-            if len(substring) > len(result_substring):
-                result_substring = substring
-            substring = ''
-            last_symbol = ''
-        return len(result_substring)
+        s = list(s)
+        list1 = []
+        max1 = 0
+        for letter in s:
+            if letter not in list1:
+                list1.append(letter)
+                max1 = max(max1, len(list1))
+            elif letter in list1:
+                sep = letter
+                thing = "".join(list1)
+                list2, sep, list1 = thing.partition(sep)
+                list1 = list(list1)
+                list1.append(letter)
+        return max1
 
 
 s = "abcabcbb"
@@ -34,6 +25,7 @@ s1 = "dvdf"
 s2 = "pwwkew"
 s3 = ' '
 s4 = 'asjrgapa'
+s5 = 'bbbbbb'
 
 sol = Solution()
 assert sol.lengthOfLongestSubstring(s) == 3
@@ -41,3 +33,4 @@ assert sol.lengthOfLongestSubstring(s1) == 3
 assert sol.lengthOfLongestSubstring(s2) == 3
 assert sol.lengthOfLongestSubstring(s3) == 1
 assert sol.lengthOfLongestSubstring(s4) == 6
+assert sol.lengthOfLongestSubstring(s5) == 1
